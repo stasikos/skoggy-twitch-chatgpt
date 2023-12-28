d3e63d60c0d457884016ce15045c75203947b75a
SKogut <stasikos@gmail.com>
2023-12-29 01:29:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/gpt/:text', async (req, res) => {
     console.log(prompt);
     
     const response = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
       prompt: prompt,
       temperature: 0.5,
       max_tokens: 128,
